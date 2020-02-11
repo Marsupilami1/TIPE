d33e9ebf9c89d2893a2740bf03be3a9861669040
Marsupilami1 <60896424+Marsupilami1@users.noreply.github.com>
2020-02-11 21:07:59 +0100

    Add files via upload

--- Carte_des_distances.py
+++ Carte_des_distances.py
@@ -9,7 +9,11 @@ def distances(matrice, x, y) : # ~ Établie la carte des distances
 	vois=[(x,y)] # ~ Liste des voisins
 	for k in range(matrice.shape[0]*matrice.shape[1]+1) : # ~ Il faudrait mieux évaluer le chemins max
 		voisins(vois, matrice, k+1) # ~ Met à jours les voisins de la zone déjà explorée
+	matrice[x][y] = 0
 	return matrice # ~ Renvoie la matrice
+
+def ingrid(x,y,c,l) :
+	return x<c and y<l and x>=0 and y>=0
 
 def voisins(liste, tab, d) : # ~ Met à jours les voisins de la zone déjà explorée
 	l,c = tab.shape # ~ Taille de la matrice
@@ -17,22 +21,66 @@ def voisins(liste, tab, d) : # ~ Met à jours les voisins de la zone déjà expl
 	for i in run : # ~ Pour toutes les cellules de la zone explorée, il faudrait restreindre à la frontière
 		x,y = i # ~ Coordonnées de la cellule en question
 		'''Ici, on regarde les quatres cellules voisines, elles prennent la valeur du nombre d'itération'''
-		if x+1<c and y<l and x+1>=0 and y>=0 and tab[x+1][y] == 0 and not ((x+1,y)in liste) :
+		if ingrid(x+1,y,c,l) and tab[x+1][y] == 0 :
 			tab[x+1][y] = d
 			liste.append((x+1,y))
-		if x<c and y-1<l and x>=0 and y-1>=0 and tab[x][y-1] == 0 and not ((x,y-1)in liste) :
+		if ingrid(x,y-1,c,l) and tab[x][y-1] == 0 :
 			tab[x][y-1] = d
 			liste.append((x,y-1))
-		if x-1<c and y<l and x-1>=0 and y>=0 and tab[x-1][y] == 0 and not ((x-1,y)in liste) :
+		if ingrid(x-1,y,c,l) and tab[x-1][y] == 0 :
 			tab[x-1][y] = d
 			liste.append((x-1,y))
-		if x<c and y+1<l and x>=0 and y+1>=0 and tab[x][y+1] == 0 and not ((x,y+1)in liste) :
+		if ingrid(x,y+1,c,l) and tab[x][y+1] == 0 :
 			tab[x][y+1] = d
 			liste.append((x,y+1))
+		# ~ Diagonales
+		if ingrid(x+1,y+1,c,l) and tab[x+1][y+1] == 0 :
+			tab[x+1][y+1] = d
+			liste.append((x+1,y+1))
+		if ingrid(x-1,y-1,c,l) and tab[x-1][y-1] == 0 :
+			tab[x-1][y-1] = d
+			liste.append((x-1,y-1))
+		if ingrid(x-1,y+1,c,l) and tab[x-1][y+1] == 0 :
+			tab[x-1][y+1] = d
+			liste.append((x-1,y+1))
+		if ingrid(x+1,y-1,c,l) and tab[x+1][y-1] == 0 :
+			tab[x+1][y-1] = d
+			liste.append((x+1,y-1))
 	# ~ Ne renvoie rien car modifie directement
 
 
-plateau = np.zeros((30,30)) # ~ Matrice 30,30
+def vitesse(distance) :
+	l,c = distance.shape
+	champ = np.zeros((l,c))
+	champ += -1
+	for y in range(l) :
+		for x in range(c) :
+			value = distance[x][y]
+			if value != 0 :
+				if ingrid(x+1,y,c,l) and distance[x+1][y] < value and distance[x+1][y] > -2 :
+					champ[x][y] = 0
+				elif ingrid(x+1,y-1,c,l) and distance[x+1][y-1] < value and distance[x+1][y-1] > -2 :
+					champ[x][y] = 1
+				elif x<c and y-1<l and x>=0 and y-1>=0 and distance[x][y-1] < value and distance[x][y-1] > -2 :
+					champ[x][y] = 2
+				elif ingrid(x-1,y-1,c,l) and distance[x-1][y-1] < value and distance[x-1][y-1] > -2 :
+					champ[x][y] = 3
+				elif x-1<c and y<l and x-1>=0 and y>=0 and distance[x-1][y] < value and distance[x-1][y] > -2 :
+					champ[x][y] = 4
+				elif ingrid(x-1,y+1,c,l) and distance[x-1][y+1] < value and distance[x-1][y+1] > -2 :
+					champ[x][y] = 5
+				elif x<c and y+1<l and x>=0 and y+1>=0 and distance[x][y+1] < value and distance[x][y+1] > -2 :
+					champ[x][y] = 6
+				elif ingrid(x+1,y+1,c,l) and distance[x+1][y+1] < value and distance[x+1][y+1] > -2 :
+					champ[x][y] = 7
+			
+	return champ
+
+
+
+
+
+plateau = np.zeros((20,20)) # ~ Matrice 20,20
 
 
 '''Pour des couloirs'''
@@ -49,12 +97,18 @@ plateau = np.zeros((30,30)) # ~ Matrice 30,30
 	# ~ plateau[1][i] = -10
 # ~ for i in range(25,0,-1) :
 	# ~ plateau[i][20] = -10
+''''''
 
-# ~ t0 = time.time()
-plateau = distances(plateau,15,15) # ~ Calcul les distances pour la sortie en 15,0
-print(time.time()-t0)
-plt.imshow(plateau) # ~ Plt fait l'imagne tout seul
-plt.show() # ~ On affiche l'image
+
+Distances_map = distances(plateau,10,10) # ~ Calcul les distances
+Vitesses_map = vitesse(Distances_map) # ~ Calcul du champ de vitesse
+print("Tableaux prets")
+
+# ~ plt.subplot(121)
+# ~ plt.imshow(Distances_map) # ~ Plt fait l'imagne tout seul
+# ~ plt.subplot(122)
+# ~ plt.imshow(Vitesses_map)
+# ~ plt.show()
 
 # ~ 10,10 -> 0.06s
 # ~ 20,20 -> 0.97s
